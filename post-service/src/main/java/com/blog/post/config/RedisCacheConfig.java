f0bda2ea363e36a5d1c43e3d3d8c7f14d5802dcc
shobhitsingh29 <shobhitsingh29@gmail.com>
2026-08-22 14:01:51 +0200

    Add Redis caching configuration and update architecture documentation

--- post-service/src/main/java/com/blog/post/config/RedisCacheConfig.java
+++ post-service/src/main/java/com/blog/post/config/RedisCacheConfig.java
@@ -3,6 +3,8 @@ package com.blog.post.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -11,6 +13,9 @@ public class RedisCacheConfig {
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration (){
-        return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(1));
+        return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(1)) .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues();
     }
 }
