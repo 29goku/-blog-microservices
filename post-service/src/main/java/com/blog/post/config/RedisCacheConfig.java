6d8d8cef81712e27ee412d91182a10a999f6e256
shobhitsingh29 <shobhitsingh29@gmail.com>
2026-08-22 14:10:22 +0200

    Enhance Redis cache configuration with custom ObjectMapper for improved serialization

--- post-service/src/main/java/com/blog/post/config/RedisCacheConfig.java
+++ post-service/src/main/java/com/blog/post/config/RedisCacheConfig.java
@@ -1,5 +1,7 @@
 package com.blog.post.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,10 +14,20 @@ import java.time.Duration;
 public class RedisCacheConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration (){
-        return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(1)) .serializeValuesWith(
+    public RedisCacheConfiguration cacheConfiguration() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.activateDefaultTyping(
+                mapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL
+        );
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(1))
+                .serializeValuesWith(
                         RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer()))
+                                new GenericJackson2JsonRedisSerializer(mapper)))
                 .disableCachingNullValues();
     }
+
 }
